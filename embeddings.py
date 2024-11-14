5c09c8720bca0aa09be3acb9ebd566147f3e02c1
yuyi <yuyi.wsy@oceanbase.com>
2024-11-14 16:40:30 +0800

    fix(image-type): restrict file type to jpg, jpeg and png

--- embeddings.py
+++ embeddings.py
@@ -17,10 +17,15 @@ def embed_img(path) -> list[float]:
     return img_pipe(path).get()[0]
 
 
+supported_img_types = [".jpg", ".jpeg", ".png"]
+
+
 def load_imgs(dir_path: str) -> Iterator[ImageData]:
     for root, _, files in os.walk(dir_path):
         for f in files:
             if f.startswith("."):
+                continue
+            if not any([f.casefold().endswith(ext) for ext in supported_img_types]):
                 continue
             file_path = os.path.abspath(os.path.join(root, f))
             embedding = embed_img(file_path)
